21e96b6360d04d4e8fdf4791ada84ac676a29b77
YifanWang <15868163725@163.com>
2020-12-16 10:08:41 +0800

    feature: 首页_tabbra

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app" class="aaa">
     <Header />
+    <Tabbar />
   </div>
 </template>
 <script>
 import Header from '@/components/Header.vue'
+import Tabbar from '@/components/Tabbar.vue'
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Tabbar
   }
 }
 </script>
---
+++ src/components/Tabbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="tabbar">
+    <router-link tag="div" class="item" to="/recommend">
+      <span>推荐</span>
+    </router-link>
+    <router-link tag="div" class="item" to="/singer">
+      <span>歌手</span>
+    </router-link>
+    <router-link tag="div" class="item" to="/rank">
+      <span>排行</span>
+    </router-link>
+    <router-link tag="div" class="item" to="/search">
+      <span>搜索</span>
+    </router-link>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped lang="scss">
+@import '~@/assets/css/mixin.scss';
+@import '~@/assets/css/variable.scss';
+.tabbar {
+  @include bg_sub_color();
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  height: 84px;
+  line-height: 84px;
+  .item {
+    @include font_color();
+    @include font_size($font_medium_s)
+  }
+  .router-link-active {
+    @include font_active_color();
+  }
+}
+</style>
